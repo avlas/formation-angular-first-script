8280d07842f0378afa76a566bca5456a68efb466
avlas <avlas@gmail.com>
2017-10-02 21:47:31 +0200

    using any

--- first-script.js
+++ first-script.js
@@ -1,4 +1,7 @@
 "use strict";
+var age1 = 30;
+age1 = '30';
+console.log("Age: " + age1);
 var age = 30;
 age = '30';
 console.log("Age: " + age);
--- first-script.ts
+++ first-script.ts
@@ -1,5 +1,7 @@
-let age:number | string = 30;
+let age1 : any = 30;
+age1 = '30';
+console.log(`Age: ${age1}`);
 
+let age : number | string = 30;
 age = '30';
-
 console.log(`Age: ${age}`);
